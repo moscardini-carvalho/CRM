d1d858c03b67a8356ebccf2ba2798fc4ce5b478a
Joao_pedro <moscardinicarvalho1234@gmail.com>
2026-04-23 23:13:11 -0300

    feat: implementar persistência com localStorage
    closes #3

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ console.log("CRM Iniciado");
 
 import { subscribe, getState } from "./state/state.js";
 import { setState } from "./state/state.js";
+import { getLeads, saveLeads } from "./services/storage.js";
 
 setTimeout(() => {
   setState({
-    leads: [{ name: "Teste" }],
+    leads: [{ name: "Gumercindo", contato: "(16)99999-9999" }],
   });
 }, 2000);
 
@@ -14,9 +15,18 @@ function render(state) {
 }
 
 function init() {
-  subscribe(render);
+  const leads = getLeads();
+  //Debugador de erro no LocalStorage
+  //Toda vez que tiver falha para armazenar
+  //Inserção de dados direto pelo console
+  /*window.setState = setState;*/
 
-  console.log("Estado inicial: ", getState());
+  setState({ leads });
+
+  subscribe((state) => {
+    saveLeads(state.leads);
+    console.log("Salvo em LocalStorage: ", state.leads);
+  });
 }
 
 init();
---
+++ services/storage.js
@@ -0,0 +1,18 @@
+const STORAGE_KEY = "crm_leads";
+
+export function saveLeads(leads) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(leads));
+}
+
+export function getLeads() {
+    const data = localStorage.getItem(STORAGE_KEY);
+
+    if (!data) return [];
+
+    try{
+        return JSON.parse(data);
+    } catch (error) {
+        console.error("Erro ao ler LocalStorage: ", error);
+        return [];
+    }
+}
